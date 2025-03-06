4f6852da65c30fc12fc3c04ecf9e97c58ffb8c3b
Ayoade Idris <ayoadeidris01@gmail.com>
2025-03-06 12:40:40 +0100

    more pages

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const barIcon = document.querySelector(".bar-icon").addEventListener("click", sh
 function showLinks () {
     
     const menuLinks = document.querySelector(".navbar-links");
+    const button = document.querySelector(".btn");
+    button.classList.toggle("show");
     menuLinks.classList.toggle("show");
+
     
 };
